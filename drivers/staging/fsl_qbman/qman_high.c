51010a10fcbb583b7836146d2167da1af2a143b9
Vladimir Oltean <vladimir.oltean@nxp.com>
2024-03-26 18:24:47 +0200

    staging: fsl_qbman: update PVB bit during drain_mr_fqrni()
    commit 6b669bd3bf7a4caea6c7681408ccafc9af88e97e from
    https://github.com/nxp-imx/linux-imx.git
    
    If FQRNI events are found in the message ring and we call
    drain_mr_fqrni(), things don't work because the code doesn't update
    the alternating Polarity Valid Bit as it should, when walking the MR.
    
    Similar to upstream commit 627da8bad530 ("soc/fsl/qbman: Fix
    drain_mr_fqni()"). This was fixed upstream but not for the SDK QBMan
    driver.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Signed-off-by: Xiaolei Wang <xiaolei.wang@windriver.com>

--- drivers/staging/fsl_qbman/qman_high.c
+++ drivers/staging/fsl_qbman/qman_high.c
@@ -526,6 +526,7 @@ static int drain_mr_fqrni(struct qm_portal *p)
 {
 	const struct qm_mr_entry *msg;
 loop:
+	qm_mr_pvb_update(p);
 	msg = qm_mr_current(p);
 	if (!msg) {
 		/* if MR was full and h/w had other FQRNI entries to produce, we
@@ -544,6 +545,7 @@ loop:
 		do {
 			now = mfatb();
 		} while ((then + 10000) > now);
+		qm_mr_pvb_update(p);
 		msg = qm_mr_current(p);
 		if (!msg)
 			return 0;
